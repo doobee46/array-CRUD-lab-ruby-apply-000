82e0e46810689036c452f54ed1efe9fbd595409f
Obed Lorisson <obed.lorisson@gmail.com>
2018-02-28 03:41:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
     array=["a","b"]
-    array << "c"
+    element = "c"
+    array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
